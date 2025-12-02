ae61e5ae542a003977a6884f69aa05ec695570c3
gaturro77 <x4nevv@gmail.com>
2025-12-02 10:25:01 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+class UserMessage(BaseModel):
+    message: str
+
+@app.post("/chat")
+def chat(data: UserMessage):
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "system", "content": "Sos un chatbot argentino y piola."},
+            {"role": "user", "content": data.message}
+        ]
+    )
+
+    reply = completion.choices[0].message.content
+    return {"reply": reply}
